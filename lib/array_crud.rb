a366e7f48c66cf45e7bcdf48bf19e1d825d9b3bc
Patrick Harrison <jahman@bellatlantic.net>
2019-07-18 02:45:50 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -3,8 +3,8 @@ def create_an_empty_array
 end
 
 def create_an_array
-   Array= ["Bob","Tom","Jim","John"]
-end
+   Array = ["Bob","Tom","Jim","John"]
+end 
 
 def add_element_to_end_of_array(array, element)
   
