4815d60814ce9fbeca9eeea61b1edd486236d6d0
Patrick Harrison <jahman@bellatlantic.net>
2019-07-18 02:58:48 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -7,7 +7,7 @@ def create_an_array
 end  
 
 def add_element_to_end_of_array(array, element)
-  array.push("arrays")
+  array.push("arrays!")
 end
 
 def add_element_to_start_of_array(array, element)
@@ -15,19 +15,19 @@ def add_element_to_start_of_array(array, element)
 end
 
 def remove_element_from_end_of_array(array)
-  
+  array.pop
 end
 
 def remove_element_from_start_of_array(array)
-  
+  array.shift
 end
 
 def retrieve_element_from_index(array, index_number)
-  
+  array[]
 end
 
 def retrieve_first_element_from_array(array)
-  
+  array[0]
 end
 
 def retrieve_last_element_from_array(array)
